d414da31a3c93197520b30acba48042cfa917b16
aalliina <90504909+aalliina@users.noreply.github.com>
2022-02-19 02:45:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const userInput = document.getElementById("userInput");
+var expression = '';
+function press(num){
+    expression += num;
+    userInput.value = expression;
+}
+function equal(){
+    userInput.value = eval(expression);
+    expression = '';
+}
+function press(num){
+    expression = '';
+    userInput.value = expression;
+}
